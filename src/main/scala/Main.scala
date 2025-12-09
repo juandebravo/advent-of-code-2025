58e0fc3285370c8ce910b702b36a458bf88f1c14
Juan de Bravo <juan.debravo@ringcentral.com>
2025-12-09 22:30:29 +0100

    implement day 2

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,12 +6,16 @@ import fs2.io.file.{Files, Path}
 
 case class Instruction(direction: Char, steps: Int)
 
+case class Range(low: Long, high: Long)
+
 object Main extends IOApp.Simple:
 
   def run: IO[Unit] =
     for
-      value <- calculateDay1("src/main/resources/day1_input.txt")
-      _ <- IO.println(value)
+      value1 <- calculateDay1("src/main/resources/day1_input.txt")
+      _ <- IO.println(value1)
+      value2 <- calculateDay2("src/main/resources/day2_input.txt")
+      _ <- IO.println(value2)
     yield ()
   
   private def buildInstructions(contents: IO[List[(Char, Int)]]): IO[List[Instruction]] =
@@ -27,7 +31,6 @@ object Main extends IOApp.Simple:
         getNewValueFromInstruction(current, instruction)
       _ <- if newValue == 0 then total.update(_ + 1) else IO.unit
       total <- total.get
-      _ <- IO.println(s"Instruction: $instruction, Current value: $newValue, Total: $total")
     yield ()
   }
 
@@ -40,12 +43,45 @@ object Main extends IOApp.Simple:
       result <- total.get
     yield result
     
-  private def readDay1File(path: String): IO[List[(Char, Int)]] =
+  private def readDay1File(input: String): IO[List[(Char, Int)]] =
     Files[IO]
-      .readAll(Path(path))
+      .readAll(Path(input))
       .through(text.utf8.decode)
       .through(text.lines)
       .collect:
         case line if line.nonEmpty => (line.head, line.tail.toInt)
       .compile
       .toList
+
+  def calculateDay2(input: String): IO[Long] =
+    for
+      contents <- readDay2File(input)
+      total <- Ref.of[IO, Long](0)
+      _ <- contents.traverseVoid(content => findInvalidIds(content, total))
+      value <- total.get
+    yield (value)
+
+  private def findInvalidIds(content: Range, total: Ref[IO, Long]): IO[Unit] =
+    val invalid = (content.low to content.high).filter(isInvalidId)
+    total.update(_ + invalid.sum)
+
+  private def isInvalidId(input: Long): Boolean = 
+    val digits = input.toString
+    val (firstHalf, secondHalf) = digits.splitAt(digits.length / 2)
+    firstHalf === secondHalf
+  
+  private def readDay2File(input: String): IO[List[Range]] = {
+
+    def buildRange(input: String): Range =
+      input.trim.split("-") match {
+        case Array(l, h) => Range(l.trim.toLong, h.trim.toLong)
+        case _ => throw new IllegalArgumentException(s"Invalid range: $input")
+      }
+
+    Files[IO]
+      .readAll(Path(input))
+      .through(text.utf8.decode)
+      .compile
+      .string
+      .map(_.split(",").map(buildRange).toList)
+  }
--- src/test/scala/AdventOfCode2025.scala
+++ src/test/scala/AdventOfCode2025.scala
@@ -4,6 +4,7 @@ import weaver._
 object AdventOfCode2025 extends SimpleIOSuite:
 
   val day1Result = 1021
+  val day2Result = 44487518055L
 
   test("make sure IO computes the right result"):
     IO.pure(1).map(_ + 2).map: result =>
@@ -12,3 +13,7 @@ object AdventOfCode2025 extends SimpleIOSuite:
   test(s"day 1 should return $day1Result"):
     Main.calculateDay1("src/main/resources/day1_input.txt").map: result =>
       expect.eql(result, day1Result)
+      
+  test(s"day 2 should return $day2Result"):
+    Main.calculateDay2("src/main/resources/day2_input.txt").map: result => 
+      expect.eql(result, day2Result)
